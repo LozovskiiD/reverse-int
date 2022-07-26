addf3f952f7fbb3ab4eb98d4ccd9ec5e92a4b173
LozovskiiD <lozdm@yandex.ru>
2022-07-27 00:10:42 +0300

    test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {    
+    c=String(Math.abs(n))
+    let b=''    
+    for (let i=c.length-1; i>=0; i--) {
+      console.log(c[i])
+      b=b+c[i]
+    }
+    return Number(b)
 }
